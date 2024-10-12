815b3e7c46cd84a500e8f4d04e87a1638d4d820a
Mohd-Amirul-Asraf-Bin-Mohamad-Nasir <amirulasraf.binmohamadnasir@etiqa.com.my>
2024-10-12 13:47:53 +0800

    feat: add build

--- vite.config.ts
+++ vite.config.ts
@@ -1,43 +1,12 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import dts from 'vite-plugin-dts'
-import { resolve, relative, extname } from 'path'
-import { fileURLToPath } from 'url'
-import { glob } from 'glob'
-const __dirname = fileURLToPath(new URL('.', import.meta.url))
+import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts(
-    {
-      insertTypesEntry: true,
-
-      include: resolve(__dirname, 'lib/**/*.{ts,tsx}')
-    }
-  )],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": "/lib"
     }
   },
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'lib/index.ts'),
-      formats: ['es']
-    }
-    ,
-    rollupOptions: {
-      external: ['react', 'react/jsx-runtime'],
-      input: Object.fromEntries(
-        glob.sync(resolve(__dirname, 'lib/**/*.{ts,tsx}')).map((file) => [
-          relative('lib', file.slice(0, file.length - extname(file).length)),
-          fileURLToPath(new URL(file, import.meta.url))
-        ])
-      ),
-      output: {
-        assetFileNames: 'assets/[name][extname]',
-        chunkFileNames: 'chunks/[name].[hash].js',
-        entryFileNames: '[name].js',
-      },
-    }
-  }
+
 })
